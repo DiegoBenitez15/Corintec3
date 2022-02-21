e12660c9f51bfda681e11fa40eef30d5ead3b3b5
nicoluv <20181669@ce.pucmm.edu.do>
2022-02-20 21:50:06 -0400

    Agregando modelos factura,pedido, orden

--- CorintecApp/models.py
+++ CorintecApp/models.py
@@ -16,6 +16,7 @@ class Distribuidor(models.Model):
     direccion = models.CharField(max_length=200)
     telefono = models.CharField(max_length=14)
 
+
 class Cargo(models.Model):
     nombre = models.CharField(max_length=30,null=True)
 
@@ -34,3 +35,61 @@ class Empleados(models.Model):
 
     def __str__(self):
         return self.nombre + ' ' + self.apellido
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=70)
+    tipo = models.CharField(max_length=30)
+    fecha_compra = models.DateField
+    fecha_venta = models.DateField
+    precio_compra = models.FloatField
+    precio_venta = models.FloatField
+    distribuidor = models.ForeignKey(Distribuidor,on_delete=models.CASCADE,null=True)
+
+class Pedido(models.Model):
+    fecha = models.DateTimeField
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE,null=True)
+
+activo= 1
+inactivo = 0
+t_estadoEnvio =((activo, 'Activo'), (inactivo, 'Inactivo'))
+
+class OrdenEnvio():
+    fecha_envio = models.DateTimeField
+    empleado = models.ForeignKey(Empleados,on_delete=models.CASCADE,null=True)
+    contactoEmpresa = models.CharField(max_length=50)
+    pedido = models.ForeignKey(Pedido,on_delete=models.CASCADE,null=True)
+    estadoEnvio = models.PositiveIntegerField(choices=t_estadoEnvio, null=True)
+
+    def orden_evio_str(self):
+        return [v[1] for v in self.t_estadoEnvio if v[0] == self.estadoEnvio][0].title()
+
+Efectivo = 0
+Credito = 1
+Deposito = 2
+t_pago = ((Efectivo, 'EFECTIVO'), (Credito, 'CREDITO'), (Deposito, 'DEPOSITO'))
+
+class Factura(models.Model):
+    fecha = models.DateField
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, null=True)
+    cantidad = models.PositiveIntegerField
+    totalPago = models.FloatField
+    subTotal = models.FloatField
+    ITBIS = models.FloatField
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True)
+    tipoPago = models.PositiveIntegerField(choices=t_pago,null=True)
+    cliente = models.ForeignKey(Pedido, on_delete=models.CASCADE, null=True)
+
+    def factura_str(self):
+        return [v[1] for v in self.t_pago if v[0] == self.tipoPago][0].title()
+
+
+
+
+
+
+
+
+
+
+
